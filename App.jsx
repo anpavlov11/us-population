6a740f9f1ea11196e43abe57507b9eec2990f8c7
anpavlov11 <apavlo11@gmail.com>
2022-05-25 21:06:12 -0500

    added React environment and app.jsx

--- App.jsx
+++ App.jsx
@@ -1,24 +1,23 @@
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import './public/styles.css';
 
 function App() {
-    const [name, setName] = useState([]);
-    useEffect(() => {
-        names()
-    }, [])
-    const names = async () => {
-        const res = await fetch('https://api.census.gov/data/2019/pep/charagegroups?get=NAME,POP&for=state:01&key=2c4b15293556d37fedd431599362146b8e6e0f7e');
-        setName(await res.json())
-    }
-    return (
-        <label>
-            <h3> {data.name} </h3>
-            <ul className = 'State'>
-                {name.map((data) => {
-                    return (
-                        <li className = 'State Demographics' key = {data.pop}> {data.name} </li>
-                    ) 
-                })}
-            </ul>
-        </label>
-    );
+  const [isShownHoverContent, setIsShownHoverContent] = useState(false);
+
+  return (
+    <div className="App">
+      <button
+        onMouseEnter={() => setIsShownHoverContent(true)}
+        onMouseLeave={() => setIsShownHoverContent(false)}>
+        Hover!
+      </button>
+      {isShownHoverContent && (
+        <div>
+          It works..!
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default App;
